4a621755166b343b75ad8fcdd89b8f85637fce25
Alexander Magola <pustotnik@gmail.com>
2021-07-01 19:58:05 +0700

    add "set -e" in the build script

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -4,6 +4,8 @@
 # One of the simplest way to use remote docker server over ssh is to use it:
 # export DOCKER_HOST=ssh://user@remote-host
 
+set -e
+
 # detect right directory and go into it
 cd "$( dirname "$(realpath ${BASH_SOURCE[0]:-$0})" )"
 
@@ -19,9 +21,9 @@ docker build -f ./md.Dockerfile \
     --build-arg USERNAME="${USERNAME}" \
     --output $OUTPUT_DIR -t $IMAGE_TAG $PROJECT_ROOT
 
-if [ $? -ne 0 ]; then
-    echo "docker build failed or interrupted"
-    exit
-fi
+#if [ $? -ne 0 ]; then
+#    echo "docker build failed or interrupted"
+#    exit
+#fi
 
 docker image prune -f
